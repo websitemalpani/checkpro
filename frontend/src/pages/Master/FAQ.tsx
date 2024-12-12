abe9d3b0546c0742e49cc135bf235fe8f3768176
Sagar <test@123>
2024-12-12 12:30:44 +0530

    the test mesg

--- frontend/src/pages/Master/FAQ.tsx
+++ frontend/src/pages/Master/FAQ.tsx
@@ -70,7 +70,7 @@ const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
             <button
               type="button"
               onClick={() => handleQuestionType('Manager')}
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-lg hover:bg-gray-900  focus:z-10 focus:ring-2 focus:ring-primary   "
+              className="inline-flex items-center px-3 mr-3 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-lg hover:bg-gray-900  focus:z-10 focus:ring-2 focus:ring-primary   "
             >
               <svg
                 className="w-3 h-3 me-2"
