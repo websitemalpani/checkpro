83fef3641e05c42add178b4ad043ee8e4d5bea1e
aakash.dighe <akashdighe2205@gmail.com>
2024-12-16 18:18:18 +0530

    env setup done

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'saragam',
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 db.connect((err) => {
   if (err) {
     console.log("Error to connecting to the db", err);
